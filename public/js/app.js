e88020a56fbfaad94bd19864a4f49167cf591ef5
IURII LYTVYN <lytvyn.139@gmail.com>
2020-05-11 16:39:31 -0500

    ninja moving up-down-left-right

--- public/js/app.js
+++ public/js/app.js
@@ -3,36 +3,67 @@
 //add option to select area
 //add fast travel home pgdown home end
 //add readme.md
-//event log aded
+//add back ground forest
+//shift + run fast
+//make ninja home
+//R = RANDOM teleport and change color
+
+
+const hero = document.getElementById("character");
+
+document.open();  
+document.write("<h1>Hello World</h1>");
+//document.write('<font color="white">'+'the ninja won\'t move out those area'+"</font>");
+document.write('<br>');
+document.write('<font color="white">'+`window size: ${window.innerWidth}px/${window.innerHeight}px`+"</font>");
+document.write('<br>');
+console.log(`${hero.leftValue}`);
+console.log(hero.style.left);
+console.log(`${document.getElementById('character').style.top}`);
+
+
+var animationFrame = 1;
 let leftValue = 450;
 let topValue = 100;
-document.write(`the ninja won't move out those area`);
-document.write('<br>');
-document.write(`width: ${window.innerWidth}px / height: ${window.innerHeight}px`);
 
- let update = () => {
-  document.getElementById("character").style.left = leftValue + "px";
-  document.getElementById("character").style.top = topValue + "px";
-}
 
 document.onkeydown = function (event) {
-  console.log(`event log: `);
-
-  console.log('%c Event log:', 'color: darkgreen; font-weight: bold', event);
+  //debug
+  console.log("%c Event log:", "color: darkgreen; font-weight: bold", event);
   const leftMoveKey = event.keyCode === 37 || event.keyCode === 65;
   const rightMoveKey = event.keyCode === 39 || event.keyCode === 68;
   const downMoveKey = event.keyCode === 40 || event.keyCode === 83;
   const upMoveKey = event.keyCode === 38 || event.keyCode === 87;
   
- 
-  if (leftMoveKey && leftValue > 50) {
-    leftValue -= 10;
-  } else if (rightMoveKey &&  leftValue < window.innerWidth - 100) {
-    leftValue += 10;
-  } else if (downMoveKey && topValue < window.innerHeight - 100 ) {
-    topValue += 10;
-  } else if (upMoveKey && topValue > 50) {
-    topValue -= 10;
+  if (animationFrame == 1) {
+    animationFrame = 2;
+  } else if (animationFrame == 2) {
+    animationFrame = 1;
   }
-  update();
+
+
+  //left move
+  if (leftMoveKey && leftValue > 20) {
+      leftValue -= 12;
+      hero.style.left = leftValue + "px";
+      hero.style.backgroundImage = "url('/public/img/left" + animationFrame + ".png')";
+
+  //right move
+  } else if (rightMoveKey && leftValue < window.innerWidth - 100) {
+      leftValue += 12;
+      hero.style.left = leftValue + "px";
+      hero.style.backgroundImage = "url('/public/img/right" + animationFrame + ".png')";
+
+  //up move
+  } else if (upMoveKey && topValue > 20) {
+      topValue -= 12;
+      hero.style.top = topValue + "px";
+      hero.style.backgroundImage = "url('/public/img/top" + animationFrame + ".png')";
+  
+  //down move
+  } else if (downMoveKey && topValue < window.innerHeight - 100) {
+      topValue += 12;
+      hero.style.top = topValue + "px";
+      hero.style.backgroundImage = "url('/public/img/down" + animationFrame + ".png')";
+  }
 };
