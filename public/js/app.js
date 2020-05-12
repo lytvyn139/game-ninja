e14c7d4a8202251bf59aaf242e9b489f34e925e5
IURII LYTVYN <lytvyn.139@gmail.com>
2020-05-11 23:58:10 -0500

    teleport added

--- public/js/app.js
+++ public/js/app.js
@@ -1,69 +1,112 @@
-//add jump
-//add canvas border line
-//add option to select area
-//add fast travel home pgdown home end
-//add readme.md
-//add back ground forest
-//shift + run fast
-//make ninja home
-//R = RANDOM teleport and change color
+//proper teleport
+//media query
+//add readme.md added dojo logo
+//html ver
+//format
+//htmk logic
+
+//speed control
+
+const hero = document.getElementById("ninja");
+let animationFrame = 1;
+let leftValue = 920;
+let topValue = 180;
+let speed = 5;
+
+function teleportModule() {
+  let randomTop = Math.floor(Math.random() * (window.innerHeight - 100));
+  let randomLeft = Math.floor(Math.random() * (window.innerWidth - 100));
+  const teleportSound = document.getElementById("teleport-mp3");
+  teleportSound.play();
+  hero.style.backgroundImage = "url('/public/img/teleport.png')";
+  console.log(
+    "%c TELEPORTING TO:",
+    "color: blue; font-weight: bold",
+    `${randomLeft}px/${randomTop}px`
+  );
+  hero.style.left = randomLeft + "px";
+  hero.style.top = randomTop + "px";
+  randomLeft = randomLeft;
+  randomTop = randomTop;
+
+}
 
 
-const hero = document.getElementById("character");
-
-document.open();  
-document.write("<h1>Hello World</h1>");
-//document.write('<font color="white">'+'the ninja won\'t move out those area'+"</font>");
-document.write('<br>');
-document.write('<font color="white">'+`window size: ${window.innerWidth}px/${window.innerHeight}px`+"</font>");
-document.write('<br>');
-console.log(`${hero.leftValue}`);
-console.log(hero.style.left);
-console.log(`${document.getElementById('character').style.top}`);
 
 
-var animationFrame = 1;
-let leftValue = 450;
-let topValue = 100;
+function speedModule() {
+  document.getElementById(
+    "stats"
+  ).innerHTML = `${window.innerWidth}px/${window.innerHeight}px`;
+  document.getElementById("speed-info").innerHTML = `speed: 05`;
+  const increaseSpeed = document.getElementById("+");
+  const decreaseSpeed = document.getElementById("-");
+  increaseSpeed.addEventListener("click", (event) => {
+    speed += 5;
+    document.getElementById("speed-info").innerHTML = `speed: ${speed}`;
+  });
 
+  decreaseSpeed.addEventListener("click", (event) => {
+    speed -= 5;
+    document.getElementById("speed-info").innerHTML = `speed: ${speed}`;
+  });
+}
 
-document.onkeydown = function (event) {
-  //debug
-  console.log("%c Event log:", "color: darkgreen; font-weight: bold", event);
-  const leftMoveKey = event.keyCode === 37 || event.keyCode === 65;
-  const rightMoveKey = event.keyCode === 39 || event.keyCode === 68;
-  const downMoveKey = event.keyCode === 40 || event.keyCode === 83;
-  const upMoveKey = event.keyCode === 38 || event.keyCode === 87;
-  
-  if (animationFrame == 1) {
-    animationFrame = 2;
-  } else if (animationFrame == 2) {
-    animationFrame = 1;
-  }
+function mainLogic() {
+  document.onkeydown = function (event) {
+    //debug
+    console.log("%c Event log:", "color: darkgreen; font-weight: bold", event);
 
+    const leftMoveKey = event.keyCode === 37 || event.keyCode === 65;
+    const rightMoveKey = event.keyCode === 39 || event.keyCode === 68;
+    const downMoveKey = event.keyCode === 40 || event.keyCode === 83;
+    const upMoveKey = event.keyCode === 38 || event.keyCode === 87;
+    const teleportKey = event.keyCode === 82;
 
-  //left move
-  if (leftMoveKey && leftValue > 20) {
-      leftValue -= 12;
+    if (animationFrame == 1) {
+      animationFrame = 2;
+    } else if (animationFrame == 2) {
+      animationFrame = 1;
+    }
+
+    //left move
+    if (leftMoveKey && leftValue > 20) {
+      leftValue -= speed;
       hero.style.left = leftValue + "px";
-      hero.style.backgroundImage = "url('/public/img/left" + animationFrame + ".png')";
+      hero.style.backgroundImage =
+        "url('/public/img/left" + animationFrame + ".png')";
 
-  //right move
-  } else if (rightMoveKey && leftValue < window.innerWidth - 100) {
-      leftValue += 12;
+      //right move
+    } else if (rightMoveKey && leftValue < window.innerWidth - 100) {
+      leftValue += speed;
       hero.style.left = leftValue + "px";
-      hero.style.backgroundImage = "url('/public/img/right" + animationFrame + ".png')";
+      hero.style.backgroundImage =
+        "url('/public/img/right" + animationFrame + ".png')";
 
-  //up move
-  } else if (upMoveKey && topValue > 20) {
-      topValue -= 12;
+      //up move
+    } else if (upMoveKey && topValue > 20) {
+      topValue -= speed;
       hero.style.top = topValue + "px";
-      hero.style.backgroundImage = "url('/public/img/top" + animationFrame + ".png')";
-  
-  //down move
-  } else if (downMoveKey && topValue < window.innerHeight - 100) {
-      topValue += 12;
+      hero.style.backgroundImage =
+        "url('/public/img/top" + animationFrame + ".png')";
+
+      //down move
+    } else if (downMoveKey && topValue < window.innerHeight - 100) {
+      topValue += speed;
       hero.style.top = topValue + "px";
-      hero.style.backgroundImage = "url('/public/img/down" + animationFrame + ".png')";
-  }
-};
+      hero.style.backgroundImage =
+        "url('/public/img/down" + animationFrame + ".png')";
+    } else if (teleportKey) {
+      setTimeout(function () {
+        teleportModule();
+      }, 1000);
+    }
+  };
+}
+function init() {
+  console.log("%c GAME STARTED 🤖", "color: darkgreen; font-weight: bold");
+  speedModule();
+  mainLogic();
+}
+
+init();
